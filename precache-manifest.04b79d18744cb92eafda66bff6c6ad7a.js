c560d19f0c66a7dac5dcc362a96f3dc7b2b5badc
hark17 <harjeevs17@gmail.com>
2020-04-23 19:20:11 +0530

    Updates

---
+++ precache-manifest.04b79d18744cb92eafda66bff6c6ad7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e6f434b48844b45f18773d7662953a",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "6995e0fd7f9c39acc7e3",
+    "url": "/covid-dashboard/static/css/main.418422ce.chunk.css"
+  },
+  {
+    "revision": "8e3cb5ef29d272d1fff3",
+    "url": "/covid-dashboard/static/js/2.22255202.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-dashboard/static/js/2.22255202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6995e0fd7f9c39acc7e3",
+    "url": "/covid-dashboard/static/js/main.9b302cfc.chunk.js"
+  },
+  {
+    "revision": "5582e4bb1d0103273fd5",
+    "url": "/covid-dashboard/static/js/runtime-main.f13b0186.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-dashboard/static/media/logo.d7265326.png"
+  }
+]);
